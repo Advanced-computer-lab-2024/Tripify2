f6cfb19ece15090daa70063bf2a8919775e00907
Oraby <youssefeloraby@gmail.com>
2024-09-30 15:43:50 +0300

    activity schema => added name activity controller =>changed create/update advertiser schema => added Document advertiser controller => changed create/update

--- server/controllers/activityController.js
+++ server/controllers/activityController.js
@@ -1,74 +1,138 @@
-const activityModel = require('../models/Activity.js'); 
+const activityModel = require("../models/Activity.js");
 
 const createActivity = async (req, res) => {
-    const { Date, Time, Location, Price, CategoryId, Tags, SpecialDiscounts,AdvertiserId, Duration} = req.body;
+  const {
+    Name,
+    Date,
+    Time,
+    Location,
+    Price,
+    CategoryId,
+    Tags,
+    SpecialDiscounts,
+    AdvertiserId,
+    Duration,
+  } = req.body;
 
-    try {
-        const newActivity = await new activityModel({ Date, Time, Location, Price, CategoryId, Tags, SpecialDiscounts,AdvertiserId, Duration });
-        await newActivity.save();
-        res.status(201).json({ message: 'Advertiser created successfully', activity: newActivity });
-    } catch (error) {
-        res.status(400).json({ message: 'Error creating advertiser', error });
-    }
-}
+  try {
+    const newActivity = await new activityModel({
+      Name,
+      Date,
+      Time,
+      Location,
+      Price,
+      CategoryId,
+      Tags,
+      SpecialDiscounts,
+      AdvertiserId,
+      Duration,
+    });
+    await newActivity.save();
+    res
+      .status(201)
+      .json({
+        message: "Advertiser created successfully",
+        activity: newActivity,
+      });
+  } catch (error) {
+    res.status(400).json({ message: "Error creating advertiser", error });
+  }
+};
 
 const getActivity = async (req, res) => {
-    try {
-        const activity = await activityModel.find(); 
-        res.status(200).json(activity);
-    } catch (error) {
-        res.status(500).json({ message: 'Error retrieving advertisers', error });
-    }
-}
+  try {
+    const activity = await activityModel.find();
+    res.status(200).json(activity);
+  } catch (error) {
+    res.status(500).json({ message: "Error retrieving advertisers", error });
+  }
+};
 
 const getActivityById = async (req, res) => {
-    const {id} = req.params;
-    //const { Username, Email, Password, Website, Hotline, Profile, Accepted } = req.body;
+  const { id } = req.params;
+  //const { Username, Email, Password, Website, Hotline, Profile, Accepted } = req.body;
 
-    try {
-        const findActivity = await activityModel.findById(id);
-        
-        if (!findActivity) {
-            return res.json({ message: 'Activity not found' });
-        }
+  try {
+    const findActivity = await activityModel.findById(id);
 
-        res.status(200).json({ message: 'Activity found', activity: findActivity });
-    } catch (error) {
-        res.status(500).json({ message: 'Error retrieving activity', error });
+    if (!findActivity) {
+      return res.json({ message: "Activity not found" });
     }
-}
+
+    res.status(200).json({ message: "Activity found", activity: findActivity });
+  } catch (error) {
+    res.status(500).json({ message: "Error retrieving activity", error });
+  }
+};
 
 const updateActivity = async (req, res) => {
-    const { id } = req.body;
-    const { Date, Time, Location, Price, CategoryId, Tags, SpecialDiscounts,AdvertiserId, Duration} = req.body;
+  const { id } = req.body;
+  const {
+    Name,
+    Date,
+    Time,
+    Location,
+    Price,
+    CategoryId,
+    Tags,
+    SpecialDiscounts,
+    AdvertiserId,
+    Duration,
+  } = req.body;
 
-    try {
-        const updatedActivity = await activityModel.findByIdAndUpdate(id, { Date, Time, Location, Price, CategoryId, Tags, SpecialDiscounts,AdvertiserId, Duration }, { new: true });
-        
-        if (!updatedActivity) {
-            return res.json({ message: 'Activity not found' });
-        }
-        
-        res.status(200).json({ message: 'Activity updated successfully', activity: updatedActivity });
-    } catch (error) {
-        res.status(400).json({ message: 'Error updating activity', error });
+  try {
+    const updatedActivity = await activityModel.findByIdAndUpdate(
+      id,
+      {
+        Name,
+        Date,
+        Time,
+        Location,
+        Price,
+        CategoryId,
+        Tags,
+        SpecialDiscounts,
+        AdvertiserId,
+        Duration,
+      },
+      { new: true }
+    );
+
+    if (!updatedActivity) {
+      return res.json({ message: "Activity not found" });
     }
-}
+
+    res
+      .status(200)
+      .json({
+        message: "Activity updated successfully",
+        activity: updatedActivity,
+      });
+  } catch (error) {
+    res.status(400).json({ message: "Error updating activity", error });
+  }
+};
 
 const deleteActivity = async (req, res) => {
-    const { id } = req.body;
+  const { id } = req.body;
 
-    try {
-        const deletedActivity = await activityModel.findByIdAndDelete(id);
-        
-        if (!deletedActivity) {
-            return res.json({ message: 'Activity not found' });
-        }
-        
-        res.status(200).json({ message: 'Activity deleted successfully' });
-    } catch (error) {
-        res.status(500).json({ message: 'Error deleting activity', error });
+  try {
+    const deletedActivity = await activityModel.findByIdAndDelete(id);
+
+    if (!deletedActivity) {
+      return res.json({ message: "Activity not found" });
     }
-}
 
-module.exports = { createActivity, getActivity, getActivityById, updateActivity, deleteActivity };
+    res.status(200).json({ message: "Activity deleted successfully" });
+  } catch (error) {
+    res.status(500).json({ message: "Error deleting activity", error });
+  }
+};
+
+module.exports = {
+  createActivity,
+  getActivity,
+  getActivityById,
+  updateActivity,
+  deleteActivity,
+};

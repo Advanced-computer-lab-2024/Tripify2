0fe6a86c884bc07fed67646efd60a67c412683c8
seifselims <124066228+seifselims@users.noreply.github.com>
2024-11-05 09:48:17 +0200

    server and utils

--- client/lib/utils.js
+++ client/lib/utils.js
@@ -2,6 +2,7 @@
 
 import clsx from "clsx"
 import { twMerge } from "tailwind-merge"
+import crg from 'country-reverse-geocoding';
 
 export function cn(...args) {
   return twMerge(clsx(...args))
@@ -39,11 +40,181 @@ export const hasErrorInput = [
 ]
 
 const usdToEur = 0.92;
-const usdToEgp = 48.59;
+const usdToEgp = 50;
 
 export const convertPrice = (price, currency) => {
   if(currency === 'USD') return price;
   if(currency === 'EUR') return (price * usdToEur).toFixed(2);
   if(currency === 'EGP') return (price * usdToEgp).toFixed(2);
   return price;
+}
+
+export const convertToUSD = (price, currency) => {
+  if(currency === 'USD') return price;
+  if(currency === 'EUR') return (price / usdToEur).toFixed(2);
+  if(currency === 'EGP') return (price / usdToEgp).toFixed(2);
+  return price;
+}
+
+const airportCodeToCity = {
+  LGW: "London Gatwick",
+  LHR: "London Heathrow",
+  STN: "London Stansted",
+  SAW: "Istanbul Sabiha Gökçen",
+  DMK: "Bangkok Don Mueang",
+  VCP: "Campinas",
+  SSA: "Salvador",
+  YVR: "Vancouver",
+  ATL: "Atlanta",
+  PEK: "Beijing",
+  LAX: "Los Angeles",
+  DXB: "Dubai",
+  HND: "Tokyo",
+  ORD: "Chicago",
+  HKG: "Hong Kong",
+  PVG: "Shanghai",
+  CDG: "Paris",
+  DFW: "Dallas",
+  AMS: "Amsterdam",
+  DEL: "Delhi",
+  CAN: "Guangzhou",
+  FRA: "Frankfurt",
+  IST: "Istanbul",
+  ICN: "Seoul",
+  SIN: "Singapore",
+  DEN: "Denver",
+  BKK: "Bangkok",
+  JFK: "New York",
+  SFO: "San Francisco",
+  CLT: "Charlotte",
+  LAS: "Las Vegas",
+  SEA: "Seattle",
+  MIA: "Miami",
+  MUC: "Munich",
+  SYD: "Sydney",
+  YYZ: "Toronto",
+  IAH: "Houston",
+  SCL: "Santiago",
+  GRU: "São Paulo",
+  MEX: "Mexico City",
+  BOM: "Mumbai",
+  MAD: "Madrid",
+  BCN: "Barcelona",
+  EZE: "Buenos Aires",
+  ZRH: "Zurich",
+  JNB: "Johannesburg",
+  KUL: "Kuala Lumpur",
+  GIG: "Rio de Janeiro",
+  PHX: "Phoenix",
+  BOS: "Boston",
+  TLV: "Tel Aviv",
+  YUL: "Montreal",
+  MEL: "Melbourne",
+  FCO: "Rome",
+  MSP: "Minneapolis",
+  ORY: "Paris",
+  CPH: "Copenhagen",
+  HEL: "Helsinki",
+  ARN: "Stockholm",
+  DUB: "Dublin",
+  VIE: "Vienna",
+  MAN: "Manchester",
+  LIS: "Lisbon",
+  DOH: "Doha",
+  BNE: "Brisbane",
+  OSL: "Oslo",
+  ATH: "Athens",
+  NRT: "Tokyo",
+  SHA: "Shanghai",
+  AKL: "Auckland",
+  EWR: "Newark",
+  YTZ: "Toronto",
+  OPO: "Paris",
+};
+
+export function getCityName(code) {
+  return airportCodeToCity[code] || code;
+}
+
+const cityToCountry = {
+  "London": "United Kingdom",
+  "New York City": "United States",
+  "Paris": "France",
+  "Tokyo": "Japan",
+  "Madrid": "Spain",
+  "Copenhagen": "Denmark",
+  "London Heathrow": "United Kingdom",
+  "Istanbul Sabiha Gökçen": "Turkey",
+  "Bangkok Don Mueang": "Thailand",
+  "Seoul Incheon": "South Korea",
+  "Campinas": "Brazil",
+  "Salvador": "El Salvador",
+  "Buenos Aires": "Argentina",
+  "Zurich": "Switzerland",
+  "Johannesburg": "South Africa",
+  "Kuala Lumpur": "Malaysia",
+  "Rio de Janeiro": "Brazil",
+  "Boston": "United States",
+  "Tel Aviv": "Israel",
+  "Mumbai": "India",
+  "Manchester": "United Kingdom",
+  "Lisbon": "Portugal",
+  "Doha": "Qatar",
+  "Brisbane": "Australia",
+  "Oslo": "Norway",
+  "Athens": "Greece",
+  "Chicago": "United States",
+  "Beijing": "China",
+  "Los Angeles": "United States",
+  "Dubai": "United Arab Emirates",
+  "Tokyo": "Japan",
+  "Toronto": "Canada",
+  "Vancouver": "Canada",
+  "Atlanta": "United States",
+  "Phoenix": "United States",
+  "Berlin": "Germany",
+  "Barcelona": "Spain",
+  "Egypt": "Egypt",
+  "Rome": "Italy",
+  "Zurich": "Switzerland",
+  "Kuala Lumpur": "Malaysia",
+  "Giza": "Egypt",
+  "Phnom Penh": "Cambodia",
+  "Brisbane": "Australia",
+  "Sydney": "Australia",
+  "Auckland": "New Zealand",
+  "Montreal": "Canada",      
+}
+
+export function getCountryName(code) {
+  return cityToCountry[code] || code;
+}
+
+export function getCountryFromCoordinates(latitude, longitude) {
+  const reverseGeocode = crg.country_reverse_geocoding();
+  try {
+      const result = reverseGeocode.get_country(latitude, longitude);
+      
+      if (result) {
+          return {
+              countryName: result.name,
+              countryCode: result.code,
+              success: true
+          };
+      }
+      
+      return {
+          countryName: null,
+          countryCode: null,
+          success: false,
+          error: 'No country found for these coordinates'
+      };
+  } catch (error) {
+      return {
+          countryName: null,
+          countryCode: null,
+          success: false,
+          error: error.message
+      };
+  }
 }

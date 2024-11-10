1bc9b0db34bf23bb9d20d38ec8e36aaf73ee15cd
adhamsameh03 <adham.sameh35@yahoo.com>
2024-11-10 06:02:41 +0200

    tourist Routes

--- server/routes/touristRoutes.js
+++ server/routes/touristRoutes.js
@@ -6,11 +6,14 @@ const {
   getTourist,
   updateTourist,
   deleteTourist,
+  redeemPoints,
 } = require("../controllers/touristController.js");
 
 const verifyTourist = require("../middleware/verifyJWT.js")
+const verifyTouristOnly = require("../middleware/verifyTouristOnly.js")
 
 router.route("/").get(getTourists).post(createTourist);
 router.route("/:id").get(getTourist).patch(verifyTourist, updateTourist).delete(verifyTourist, deleteTourist);
+router.route("/points/redeem").post(verifyTouristOnly, redeemPoints);
 
 module.exports = router;

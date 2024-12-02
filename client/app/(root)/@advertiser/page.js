7ac631f3a3144767edaa144d91ae930123a861b7
HanaEldemery <hanaeldemery2002@gmail.com>
2024-12-02 21:39:05 +0200

    hanoozi

--- client/app/(root)/@advertiser/page.js
+++ client/app/(root)/@advertiser/page.js
@@ -1,49 +1,24 @@
-"use client";
-import Dashboard from "@/components/ui/dashboard";
+import MyActivitiesAdvertiser from "@/components/ui/MyActivitiesAdvertiser.js";
+import { getSession } from "@/lib/session";
+import { fetcher } from "@/lib/fetch-client";
 
-import { useRouter } from "next/navigation";
+export default async function MyActivities() {
+  const res = await fetcher(`/advertisers/get-all/my-activities`, {
+    method: "GET",
+    headers: {
+      "Content-Type": "application/json",
+    },
+  }).catch((e) => console.log(e));
+  if (!res?.ok) {
+    console.log("err");
+  }
 
-export default function MyAdvertiser() {
-  const router = useRouter();
+  const Activities = await res.json();
+  // console.log(Activities);
 
-  const handleProfileClick = () => {
-    router.push("/profile");
-  };
-
-  const handleActivitiesClick = () => {
-    router.push("/my-activities");
-  };
-  const handleReportClick = () => {
-    router.push("/sales-report");
-  };
   return (
     <div>
-      <header>
-        <Dashboard params={{ role: "Advertiser" }} />
-      </header>
-      <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-        <h1 className="text-3xl font-bold">Welcome to MyAdvertiser</h1>
-        <div className="flex flex-col gap-4">
-          <button
-            className="px-6 py-3 bg-blue-500 text-white rounded hover:bg-blue-700"
-            onClick={handleProfileClick}
-          >
-            View My Profile
-          </button>
-          <button
-            className="px-6 py-3 bg-green-500 text-white rounded hover:bg-green-700"
-            onClick={handleActivitiesClick}
-          >
-            My Activities
-          </button>
-          <button
-            className="px-6 py-3 bg-green-500 text-white rounded hover:bg-green-700"
-            onClick={handleReportClick}
-          >
-View Sales Report          
-</button>
-        </div>
-      </div>
+      <MyActivitiesAdvertiser Activities={Activities} />
     </div>
   );
 }

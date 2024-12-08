1c84c6721e395f6f3ac8f7162f194995612d6dba
omarayman59 <omarayman40@gmail.com>
2024-12-08 15:13:06 +0200

    small

--- client/app/(root)/@guest/places-guest/page.js
+++ client/app/(root)/@guest/places-guest/page.js
@@ -187,6 +187,8 @@ const PlacesPage = () => {
                         </Badge>
                       ) : null;
                     })}
+                  </p>
+                  <p>
                     {place.Categories.map((categoryId) => {
                       const foundCategory = categories.find(
                         (actualCategory) => actualCategory._id === categoryId
